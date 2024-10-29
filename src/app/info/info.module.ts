c43c2130840895133e2bbb943692f82f343dfb9f
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2024-10-29 15:41:35 -0300

    Traduzindo páginas de contato e política do repositório para inglês

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -13,7 +13,10 @@ import { FeedbackFormComponent } from './feedback/feedback-form/feedback-form.co
 import { ThemedFeedbackFormComponent } from './feedback/feedback-form/themed-feedback-form.component';
 import { ThemedFeedbackComponent } from './feedback/themed-feedback.component';
 import { FeedbackGuard } from '../core/feedback/feedback.guard';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { RepositoryPolicyPageComponent } from './repository-policy-page/repository-policy-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PresentationPageComponent } from './presentation-page/presentation-page.component';
 
 const DECLARATIONS = [
   EndUserAgreementComponent,
@@ -25,7 +28,10 @@ const DECLARATIONS = [
   FeedbackComponent,
   FeedbackFormComponent,
   ThemedFeedbackFormComponent,
-  ThemedFeedbackComponent
+  ThemedFeedbackComponent,
+  RepositoryPolicyPageComponent,
+  AboutPageComponent,
+  PresentationPageComponent
 ];
 
 @NgModule({
@@ -33,6 +39,7 @@ const DECLARATIONS = [
     CommonModule,
     SharedModule,
     InfoRoutingModule,
+    TranslateModule,
   ],
   declarations: [
     ...DECLARATIONS
