361d0b1f93b8d7138d324838eb9d6b460b142041
Francisco <francisco.carvalho.q@gmail.com>
2024-10-29 15:40:14 -0300

    Reduzindo duplicação de código em itens-card

--- src/themes/deposita/app/home-page/itens-card/itens-card.component.ts
+++ src/themes/deposita/app/home-page/itens-card/itens-card.component.ts
@@ -8,47 +8,31 @@ import { environment } from 'src/environments/environment';
 })
 
 export class ItensCardComponent implements OnInit {
-  artigo_revista_qtd = 0;
-  artigo_conferencia_qtd = 0;
-  livro_qtd = 0;
-  capitulo_livro_qtd = 0;
-  tcc_qtd = 0;
-  tese_qtd = 0;
-  dissertacao_qtd = 0;
+  quantidades = {}
+  objectEntries = Object.entries
+  iconePadrao = "assets/deposita/images/li_file.svg"
+  icones = {
+    "Artigo": "assets/deposita/images/li_file.svg",
+    "Conference": "assets/deposita/images/text-cursor.svg",
+    "Livro": "assets/deposita/images/book.svg",
+    "Capítulo de livro": "assets/deposita/images/bookmark.svg",
+    "Trabalho de conclusão de curso": "assets/deposita/images/book.svg",
+    "Tese": "assets/deposita/images/edit.svg",
+    "Dissertação": "assets/deposita/images/quote.svg",
+  }
 
   async getItensQtd(): Promise<any> {
-    const tiposItens = ['Article', 'conference_object', 'book', 'book_part', 'Trabalho_de_conclusao_de_curso', 'thesis', 'Dissertação'];
+    const tiposItens = ["Artigo", "Conference", "Livro", "Capítulo de livro", "Trabalho de conclusão de curso", "Tese", "Dissertação"];
+
     const promises = tiposItens.map(tipo => {
       return fetch(environment.apiUrl + `/server/api/discover/facets/has_content_in_original_bundle?query=dc.type:${tipo}`).then(res => res.json());
     });
     const results = await Promise.all(promises);
-    // console.log(results[0]._embedded.values[0].count);
 
     // Atualiza as variáveis com a quantidade de cada tipo de item
     results.forEach((result, index) => {
-      switch (tiposItens[index]) {
-        case 'Article':
-          this.artigo_revista_qtd = result._embedded.values[0].count;
-          break;
-        case 'conference_object':
-          this.artigo_conferencia_qtd = result._embedded.values[0].count;
-          break;
-        case 'book':
-          this.livro_qtd = result._embedded.values[0].count;
-          break;
-        case 'book_part':
-          this.capitulo_livro_qtd = result._embedded.values[0].count;
-          break;
-        case 'Trabalho_de_conclusao_de_curso':
-          this.tcc_qtd = result._embedded.values[0].count;
-          break;
-        case 'thesis':
-          this.tese_qtd = result._embedded.values[0].count;
-          break;
-        case 'Dissertação':
-          this.dissertacao_qtd = result._embedded.values[0].count;
-          break;
-      }
+      const nomeDaColecao: string = tiposItens[index];
+      this.quantidades[nomeDaColecao] = result._embedded.values[0]?.count ?? 0;
     });
   }
 
