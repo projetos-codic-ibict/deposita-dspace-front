c3b1df755b9446f43d1079e2e2e02f74f2adeb18
Jesiel Viana <jesielviana@proton.me>
2024-11-20 17:07:00 -0300

    alterando o processo de deploy automatizado

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,23 @@ set -e
 echo "node version >>"
 node -v
 
-echo "running yarn install"
-yarn
-
 echo "Running pm2 stop deposita"
 pm2 stop deposita || true
+
+echo "running yarn install"
+yarn
 
 echo "copy env"
 rm config/config.prod.yml ||  true
 cp config/config.example.yml  config/config.prod.yml
 
+echo "yarn build"
+yarn build:prod
+
 echo "yarn deploy"
 yarn deploy
 
 echo "pm2 save"
-pm2 save 
+pm2 save
 
 echo "finished!"
